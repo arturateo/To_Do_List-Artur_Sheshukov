2b64cacd8e3fc6a9f7f8f8b1c732340507e152b1
Artur Sheshukov <arti.shishka11@gmail.com>
2023-06-18 21:35:52 +0600

    Изменил модель todolist чтобы можно было вписать пустую дату, изменил представление в админке

--- todolist/admin.py
+++ todolist/admin.py
@@ -2,14 +2,18 @@ from django.contrib import admin
 from todolist.models import ToDoList
 
 
-admin.site.register(ToDoList)
-
-
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ['id', 'description', 'status', 'date_of_completion']
+    list_display_links = ['description']
     list_filter = ['status']
     search_fields = ['description', 'status']
     fields = ['description', 'status', 'date_of_completion']
+
+
+admin.site.register(ToDoList, ArticleAdmin)
+
+
+
 
 
 
--- todolist/models.py
+++ todolist/models.py
@@ -7,7 +7,7 @@ class ToDoList(models.Model):
     description = models.TextField(max_length=3000, null=False, blank=False, verbose_name='Описание задачи')
     status = models.CharField(max_length=40, null=False, blank=False, default='new', verbose_name='Статус',
                               choices=status_choices)
-    date_of_completion = models.DateField(null=True, blank=True, default='none', verbose_name='Дата выполнения')
+    date_of_completion = models.DateField(null=True, blank=True, verbose_name='Дата выполнения')
 
     def __str__(self):
         return f'{self.pk} {self.description}, {self.status}, {self.date_of_completion}'
