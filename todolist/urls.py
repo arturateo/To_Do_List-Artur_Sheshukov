a4805691d682fd5aa76a22d3b299d4d5a24b8335
Artur Sheshukov <arti.shishka11@gmail.com>
2023-06-20 23:51:56 +0600

    Начал создавать базовый шаблон, добавил именна к ендпоинтам

--- todolist/urls.py
+++ todolist/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from todolist.views import home, add_tasks, delete_task
 
 urlpatterns = [
-    path('', home),
-    path('add/', add_tasks),
-    path('delete/', delete_task)
+    path('', home, name='home'),
+    path('add/', add_tasks, name='add_task'),
+    path('delete/', delete_task, name='delete_task')
 ]
