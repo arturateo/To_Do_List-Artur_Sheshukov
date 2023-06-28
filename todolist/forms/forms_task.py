4b3b4dbca370346d872606f9dc992ad4940bf5e0
Artur Sheshukov <arti.shishka11@gmail.com>
2023-06-28 17:27:48 +0600

    С помощью формы Django изменил шаблоны добавления и редактирования задачи

--- todolist/forms/forms_task.py
+++ todolist/forms/forms_task.py
@@ -5,11 +5,11 @@ from todolist.models import status_choices
 
 
 class TaskForm(forms.Form):
-    title = forms.CharField(max_length=200, required=True, label='Название задачи',
-                            widget=forms.TextInput(attrs={"class": "form-control"}))
-    description = forms.CharField(max_length=40, required=True, label='Описание задачи', widget=widgets.Textarea(
-                                  attrs={"class": "form-control"}))
+    name_task = forms.CharField(max_length=100, required=True, label='Название задачи',
+                                widget=forms.TextInput(attrs={"class": "form-control"}))
+    description = forms.CharField(max_length=3000, required=True, label='Описание задачи', widget=widgets.Textarea(
+        attrs={"class": "form-control"}))
     status = forms.ChoiceField(required=True, label='Статус', choices=status_choices,
                                widget=forms.Select(attrs={"class": "form-control"}))
-    data = forms.DateField(required=False, label='Дата завершения', widget=widgets.DateInput(
-                           attrs={"class": "form-control"}))
+    date_of_completion = forms.DateField(required=False, label='Дата завершения', widget=widgets.DateInput(
+        attrs={"class": "form-control", 'type': 'date'}))
--- todolist/views.py
+++ todolist/views.py
@@ -13,23 +13,27 @@ def home(request):
 def add_tasks(request):
     if request.method == "GET":
         form = TaskForm()
-        datas = {'lists': status_choices}
         return render(request, 'add_tasks.html', {"form": form})
     else:
-        date_of_completion = request.POST.get('date_of_completion')
-        if not date_of_completion:
-            date_of_completion = None
-        ToDoList.objects.create(name_task=request.POST.get('name_task'),
-                                description=request.POST.get('description'),
-                                status=request.POST.get('status'),
-                                date_of_completion=date_of_completion)
-        return redirect('home')
+        form = TaskForm(data=request.POST)
+        if form.is_valid():
+            date_of_completion = form.cleaned_data['date_of_completion']
+            if not date_of_completion:
+                date_of_completion = None
+            todolist = ToDoList.objects.create(
+                name_task=form.cleaned_data['name_task'],
+                description=form.cleaned_data['description'],
+                status=form.cleaned_data['status'],
+                date_of_completion=date_of_completion)
+            return redirect('detail_task', pk=todolist.pk)
+        else:
+            return render(request, 'add_tasks.html', {'form': form})
 
 
 def detail_task(request, pk):
     to_do_list = get_object_or_404(ToDoList, pk=pk)
     data = {'task': to_do_list, 'choices': status_choices}
-    return render(request, 'detail.html', data)
+    return render(request, 'detail_task.html', data)
 
 
 def delete_task(request, pk):
@@ -39,5 +43,26 @@ def delete_task(request, pk):
 
 
 def edit_task(request, pk):
-    to_do_list = get_object_or_404(ToDoList, pk=pk)
-    return render(request, 'edit.html', {"to_do_list": to_do_list})
+    todolist = get_object_or_404(ToDoList, pk=pk)
+    if request.method == "GET":
+        form = TaskForm(initial={
+            'name_task': todolist.name_task,
+            'description': todolist.description,
+            'status': todolist.status,
+            'date_of_completion': todolist.date_of_completion,
+        })
+        return render(request, 'edit_task.html', {'form': form, 'todolist': todolist})
+    else:
+        form = TaskForm(data=request.POST)
+        if form.is_valid():
+            date_of_completion = form.cleaned_data['date_of_completion']
+            if not date_of_completion:
+                date_of_completion = None
+            todolist.name_task = form.cleaned_data['name_task']
+            todolist.description = form.cleaned_data['description']
+            todolist.status = form.cleaned_data['status']
+            todolist.date_of_completion = date_of_completion
+            todolist.save()
+            return redirect('detail_task', pk=todolist.pk)
+        else:
+            return render(request, 'edit_task.html', {'form': form, 'todolist': todolist})
